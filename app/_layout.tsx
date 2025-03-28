d7673c558de7025788307db786095bee342a4da1
gabiapc <gabi.apucaloaiei@gmail.com>
2025-03-28 11:10:01 +0200

    fetch latest movies and search by name

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
 import { Stack } from 'expo-router';
 import './globals.css';
+import { StatusBar } from 'react-native';
 
 export default function RootLayout() {
 	return (
 		<Stack>
-			<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+			<Stack.Screen
+				name='(tabs)'
+				options={{
+					headerShown: false,
+
+					statusBarStyle: 'light',
+					statusBarTranslucent: true,
+					statusBarHidden: false,
+					statusBarBackgroundColor: 'transparent',
+				}}
+			/>
 			<Stack.Screen name='movies/[id]' options={{ headerShown: false }} />
 		</Stack>
 	);
---
+++ components/SearchBar.tsx
@@ -0,0 +1,33 @@
+import { View, Text, Image, TextInput } from 'react-native';
+import React from 'react';
+import { icons } from '@/constants/icons';
+import { on } from 'events';
+
+interface Props {
+	placeholder: string;
+	onPress?: () => void;
+	value: string;
+	onChangeText?: (text: string) => void;
+}
+const SearchBar = ({ placeholder, onPress, value, onChangeText }: Props) => {
+	return (
+		<View className='flex-row items-center bg-[#0f0D23] rounded-full px-5 py-3'>
+			<Image
+				source={icons.search}
+				resizeMode='contain'
+				className='size-5'
+				tintColor='#ab8bff'
+			/>
+			<TextInput
+				className='flex-1 ml-2 text-white'
+				placeholder={placeholder}
+				placeholderTextColor='#a8b5db'
+				onPress={onPress}
+				value={value}
+				onChangeText={onChangeText}
+			/>
+		</View>
+	);
+};
+
+export default SearchBar;
